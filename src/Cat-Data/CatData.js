7d4b310aab71814d4b413b1cc4c2924055dbe8fa
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-12-24 14:52:13 +0200

    add new type of cat with link to fix netlify proplem

--- src/Cat-Data/CatData.js
+++ src/Cat-Data/CatData.js
@@ -15,57 +15,57 @@
     {
         "id":  1  ,
         "price": 100,
-        "name": " Amercian cute  ",
-        "Image": "/images/american1.png",
+        "name": " white Exotic shorthair" , 
+        "Image": "https://www.bellamiacattery.com/pictures/fairytale12.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
 
       {
         "id": 9,
         "price": 250,
-        "name": " Scotish cat  ",
-        "Image": "/images/640px-Lilac_Scottish_Fold.png",
+        "name": " Brown Exotic shorthair  ",
+        "Image": "https://www.arageek.com/wp-content/uploads/2018/05/%D9%82%D8%B7-%D8%A7%D9%84%D8%AF%D8%AE%D9%8A%D9%84.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
 
       {
         "id": 2,
-        "name": "American Cat  ",
+        "name": " Green Exotic shorthair  ",
         "price": 150,
-        "Image": "/images/american2.png",
+        "Image": "https://www.wideopenpets.com/wp-content/uploads/2020/06/Untitled-design-2020-06-29T173735.102.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
 
       {
         "id": 3,
-        "name": "Bengal cat",
+        "name": " Gray briths shorthair  ",
         "price": 300,
-        "Image": "/images/Baldie1.png",
+        "Image": "https://www.townoflaronge.ca/wp-content/uploads/2021/10/blackcat-1024x576.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
 
       {
         "id": 4,
-        "name": "Britsh cat ",
+        "name": " white britsh cat ",
         "price": 350,
-        "Image": "/images/Britsh1.png",
+        "Image": "https://static.wixstatic.com/media/9d3609_eb9f59b5f7c64d93a5686d606094011e~mv2.jpg/v1/fill/w_640,h_504,al_c,q_80,usm_0.66_1.00_0.01/9d3609_eb9f59b5f7c64d93a5686d606094011e~mv2.webp" ,
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
 
       {
         "id": 5 ,
-        "name": "Britsh gray  cat ",
+        "name": " Black britsh cat  ",
         "price": 400,
-        "Image": "/images/britsh2.png",
+        "Image": "https://s3.envato.com/files/192661200/preview.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       },  
 
       
       {
         "id": 6 ,
-        "name": "kitten  ",
+        "name": " Green shirazie cat  ",
         "price": 40 ,
-        "Image": "/images/cute1.png",
+        "Image": "https://www.thesprucepets.com/thmb/-2TISPth6ekCcSrV21b1F5xlcSM=/3864x2173/smart/filters:no_upscale()/brown-persian-cat-laying-on-edge-of-green-carpet-456750383-581211b53df78c2c7320b40b.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       },  
 
@@ -74,16 +74,16 @@
         "id": 7 ,
         "name": "Shirazie cat eva   ",
         "price": 480,
-        "Image": "/images/sh1.png",
+        "Image": "https://vetwork.co/galaxy/wp-content/uploads/%D8%A7%D9%84%D9%82%D8%B7%D8%B7-%D8%A7%D9%84%D8%B4%D9%8A%D8%B1%D8%A7%D8%B2%D9%8A-5.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       },  
 
       
       {
         "id": 8 ,
-        "name": " shirazie cat  ",
+        "name": " shirazie cat zeno  ",
         "price": 450,
-        "Image": "/images/sh2.png",
+        "Image": "https://mummycat.in/wp-content/uploads/2018/12/persian-cat.jpg",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
     
