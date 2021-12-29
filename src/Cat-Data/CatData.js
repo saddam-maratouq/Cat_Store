e63e63e31c5f924b06f0cdf297ac7936d2a75d26
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-12-29 08:42:16 +0200

    still need to find new way to display image on netlify

--- src/Cat-Data/CatData.js
+++ src/Cat-Data/CatData.js
@@ -16,8 +16,8 @@
         "id":  1  ,
         "price": 100,
         "name": " white Exotic shorthair" , 
-        // "Image": "https://www.bellamiacattery.com/pictures/fairytale12.jpg",
-        "Image": "./src/Images/ferarie.jpg",
+        "Image": "https://www.bellamiacattery.com/pictures/fairytale12.jpg",
+        // "Image": "./src/Images/ferarie.jpg",
 
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       }, 
