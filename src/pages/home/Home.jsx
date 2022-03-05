0edccf27da9400acaa7f6f3ce01d9a1b84467753
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-03-04 22:41:34 -0500

    fix slide size

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,59 +6,82 @@ import Carousel from 'react-bootstrap/Carousel'
 
 //img 
 
-import cat1 from '../../Images/herocat.jpg' 
+import cat1 from '../../Images/cat1.jpg' 
 
-import cat2 from '../../Images/cat2.jpg' 
+import cat2 from '../../Images/cat2.jpg'  
 
 import cat3 from '../../Images/cat3.jpg' 
+
+
+
 
 
    const Home = () => {
     return (
     
        
-    <section>
+    <section  className='main'>
 
                 <Carousel fade variant="white" 
                 
                 autoPlay={true}
-                interval={4000}
+                interval={4000} 
               
-                >
-                  <Carousel.Item>
-                    <img
-                      className="d-block w-100"
+                > 
+
+                 
+
+                  <Carousel.Item> 
+
+                  <img
+                      className="d-block  cat-img "
                       src={cat1}
                       alt="First slide"
-                    />
-                    <Carousel.Caption>
-                      <h5>First slide label</h5>
-                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                  <Carousel.Item>
+                    /> 
+
+                  <Carousel.Caption className='f-label'  >
+                   
+                   <div className='info'>
+                     <p>your one stop </p>
+                     <p > cats shop</p>
+                   </div>
+                  
+                   </Carousel.Caption> 
+                 
+                
+                  </Carousel.Item> 
+
+                   <Carousel.Item>
                     <img 
-                      className="d-block w-100"
+                      className="d-block   cat-img"
                       src={cat2} 
                       alt="Second slide"
                     />
-                    <Carousel.Caption>
-                      <h5>Second slide label</h5>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                     <Carousel.Caption className='f-label'  >
+                   
+                   <div className='info'>
+                     <p>your one stop </p>
+                     <p > cats shop</p>
+                   </div>
+                  
+                   </Carousel.Caption> 
                   </Carousel.Item>
                   <Carousel.Item>
                     <img
-                      className="d-block w-100"
+                      className="d-block cat-img"
                       src={cat3}  
                       alt="Third slide"
                     />
-                    <Carousel.Caption>
-                      <h5>Third slide label</h5>
-                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                     <Carousel.Caption className='f-label'  >
+                   
+                   <div className='info'>
+                     <p>your one stop </p>
+                     <p > cats shop</p>
+                   </div>
+                  
+                   </Carousel.Caption> 
                   </Carousel.Item>
-                </Carousel>
+                </Carousel> 
 
     </section>
 
