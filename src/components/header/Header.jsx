bad874532d098d765767d8c500eb798db63f47cc
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-04-04 17:56:15 -0400

    back to previous code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='list' className="collapse navbar-collapse  " id="navbarSupportedContent">
+        <div  className="collapse navbar-collapse  " id="navbarSupportedContent">
           <ul className="navbar-nav  ms-auto px-5  mb-2  mb-lg-0"> 
           
           <li className="nav-item">
