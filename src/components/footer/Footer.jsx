fb7cdec39248b966c5773d44299c9a267a646ce5
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-12-02 20:47:53 +0300

    add footer-color

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,69 +1,109 @@
-import React from 'react' 
+import React from "react";
 
-import './footer.css'
+import "./footer.css";
 
-//icon 
-
-
-
+//icon
 
 const Footer = () => {
   return (
+    <footer class="footer-content d-lg-flex ">
+      <div class="container">
+        <div class="row footer-link  ">
+          <div class="col-md-4 pr-md-5">
+            <a href="#" class="footer-site-logo d-block mb-4">
+              SQ Cat
+            </a>
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quasi perferendis ratione perspiciatis accusantium.</p> */}
+          </div>
+          <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li>
+                <a href="#">Clients</a>
+              </li>
+              <li>
+                <a href="#">Team</a>
+              </li>
+              <li>
+                <a href="#">Career</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+              <li>
+                <a href="#">Journal</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms &amp; Conditions</a>
+              </li>
+              <li>
+                <a href="#">Partners</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md text-md-center">
+            <ul class="social list-unstyled d-md-flex justify-content-evenly  ">
+              <li>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa-brands fa-pinterest"></i>
+                </a>
+              </li>
+            </ul>
+            {/* <p class=""><a href="#" class=" btn-info btn-footer ">Contact</a></p> */}
+          </div>
+        </div>
+        <div class="row-f ">
+          <div class="col-12 text-center">
+            <div class="copyright mt-5 pt-5">
+              <p>
+                <small> © 2022 Copyright:</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-
-<footer class="footer-content d-lg-flex ">
-<div class="container">
-<div class="row">
-<div class="col-md-4 pr-md-5">
-<a href="#" class="footer-site-logo d-block mb-4">SQ Cat</a>
-{/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quasi perferendis ratione perspiciatis accusantium.</p> */}
-</div>
-<div class="col-md">
-<ul class="list-unstyled nav-links">
-<li><a href="#">Home</a></li>
-<li><a href="#">About Us</a></li>
-<li><a href="#">Portfolio</a></li>
-<li><a href="#">Services</a></li>
-<li><a href="#">Contact</a></li>
-</ul>
-</div>
-<div class="col-md">
-<ul class="list-unstyled nav-links">
-<li><a href="#">Clients</a></li>
-<li><a href="#">Team</a></li>
-<li><a href="#">Career</a></li>
-<li><a href="#">Testimonials</a></li>
-<li><a href="#">Journal</a></li>
-</ul>
-</div>
-<div class="col-md">
-<ul class="list-unstyled nav-links">
-<li><a href="#">Privacy Policy</a></li>
-<li><a href="#">Terms &amp; Conditions</a></li>
-<li><a href="#">Partners</a></li>
-</ul>
-</div>
-<div class="col-md text-md-center">
-<ul class="social list-unstyled d-md-flex justify-content-evenly  ">
-<li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-<li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-<li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-<li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
-</ul>
-{/* <p class=""><a href="#" class=" btn-info btn-footer ">Contact</a></p> */}
-</div>
-</div>
-<div class="row-f ">
-<div class="col-12 text-center">
-<div class="copyright mt-5 pt-5">
-<p><small>  © 2022 Copyright:</small></p>
-</div>
-</div>
-</div>
-</div>
-</footer>
-   ) 
-}
-
-export default Footer
-      
+export default Footer;
